adf9ba9d7ab263ba968d2b7e80d2158cac199660
Suneel Kumar Khatri <khatrisuneel96@gmail.com>
2020-07-19 23:16:16 +0500

    Component part 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap'
+import Menu from './components/MenuComponent'
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <Navbar dark color="primary">
             <div className="container">
                 <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
             </div>
         </Navbar>
+        <Menu />
     </div>
   );
 }
